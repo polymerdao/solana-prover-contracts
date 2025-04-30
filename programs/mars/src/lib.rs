ae314548087ebdbf6c28fb4a584075f64747bb09
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2025-04-30 18:48:41 +0200

    add mars program

---
+++ programs/mars/src/lib.rs
@@ -0,0 +1,47 @@
+#![allow(unexpected_cfgs)]
+use anchor_lang::prelude::*;
+
+declare_id!("Et42Hf56gX2ABd7AHkKHMP8Ckh3LYF5CvPUKUG6g8qxy");
+
+const DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+mod mars {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        msg!("Prove: Initialized");
+        Ok(())
+    }
+
+    pub fn set_data(ctx: Context<SetData>, data: Data) -> Result<()> {
+        let data_account = &mut ctx.accounts.data;
+        data_account.data = data.data;
+        // DO NOT REMOVE
+        // this msg is what we end up generating a proof for!
+        msg!("Prove: Data: {}", data_account.data);
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(Default, InitSpace)]
+pub struct Data {
+    #[max_len(32)]
+    pub data: String,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = DISCRIMINATOR_SIZE + Data::INIT_SPACE)]
+    pub data: Account<'info, Data>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info> {
+    #[account(mut)]
+    pub data: Account<'info, Data>,
+}
