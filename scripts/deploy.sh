7bd0449f17e557873d668c30ac72f501c6367afc
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2025-12-01 23:19:16 +0100

    fix up deployment tooling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
 # use this so the script can be run from anywhere
 ROOT="$(realpath "$(dirname "$(realpath "$0")")"/..)"
@@ -10,8 +10,8 @@ check_env() {
 		echo "PROGRAM_KEYPAIR_FILE env variable is not set" >&2
 		exit 1
 	fi
-	if [ -z "$CLUSTER" ]; then
-		echo "CLUSTER env variable is not set" >&2
+	if [ -z "$POLYMER_ENV" ]; then
+		echo "POLYMER_ENV env variable is not set" >&2
 		exit 1
 	fi
 	if [ -z "$TYPE" ]; then
@@ -34,13 +34,21 @@ main() {
 	# install the solana sdk
 	. "$ROOT/scripts/solana.sh"
 
-	# testnet is really devnet
-	local cluster="$CLUSTER"
-	if [ "$cluster" = 'testnet' ]; then
-		cluster='devnet'
-	fi
+	local solana_cluster=''
+
+	case "$POLYMER_ENV" in
+	devnet | testnet) solana_cluster='devnet' ;;
+	shadownet | mainnet) solana_cluster='mainnet-beta' ;;
+	*)
+		echo "unknown POLYMER_ENV '$POLYMER_ENV'" >&2
+		exit 1
+		;;
+	esac
+
+	echo "> using polymer environment '$POLYMER_ENV' and solana cluster '$solana_cluster'"
+
 	# this takes care of checking if the cluster is valid
-	solana config set --url "$cluster"
+	solana config set --url "$solana_cluster"
 
 	solana config set --keypair "$KEYPAIR_FILE"
 
@@ -66,7 +74,7 @@ main() {
 		exit 1
 	fi
 
-	echo "> deploying '$so_file' to '$CLUSTER'"
+	echo "> deploying '$so_file' to solana cluster '$solana_cluster'"
 
 	# finally, deploy the program
 	solana program deploy \
